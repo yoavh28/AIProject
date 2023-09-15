1cad02eb94c09bdfe86b6aa09b843e1a11fa75ed
yoavh28 <55988645+yoavh28@users.noreply.github.com>
2023-09-15 14:32:20 +0300

    Add files via upload

---
+++ practice/music_genre_classification.py
@@ -0,0 +1,420 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+get_ipython().system('pip install librosa')
+
+
+# In[1]:
+
+
+import numpy as np
+import librosa, librosa.display
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+import numpy as np
+import librosa, librosa.display
+import matplotlib.pyplot as plt
+
+FIG_SIZE = (15,10)
+
+file = "blues.00000.wav"
+
+# load audio file with Librosa
+signal, sample_rate = librosa.load(file, sr=22050)
+
+# WAVEFORM
+# display waveform
+plt.figure(figsize=FIG_SIZE)
+librosa.display.waveshow(signal, alpha=0.4)
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude")
+plt.title("Waveform")
+
+
+# FFT -> power spectrum
+# perform Fourier transform
+fft = np.fft.fft(signal)
+
+# calculate abs values on complex numbers to get magnitude
+spectrum = np.abs(fft)
+
+# create frequency variable
+f = np.linspace(0, sample_rate, len(spectrum))
+
+# take half of the spectrum and frequency
+left_spectrum = spectrum[:int(len(spectrum)/2)]
+left_f = f[:int(len(spectrum)/2)]
+
+# plot spectrum
+plt.figure(figsize=FIG_SIZE)
+plt.plot(left_f, left_spectrum, alpha=0.4)
+plt.xlabel("Frequency")
+plt.ylabel("Magnitude")
+plt.title("Power spectrum")
+
+
+# STFT -> spectrogram
+hop_length = 512 # in num. of samples
+n_fft = 2048 # window in num. of samples
+
+# calculate duration hop length and window in seconds
+hop_length_duration = float(hop_length)/sample_rate
+n_fft_duration = float(n_fft)/sample_rate
+
+print("STFT hop length duration is: {}s".format(hop_length_duration))
+print("STFT window duration is: {}s".format(n_fft_duration))
+
+# perform stft
+stft = librosa.stft(signal, n_fft=n_fft, hop_length=hop_length)
+
+# calculate abs values on complex numbers to get magnitude
+spectrogram = np.abs(stft)
+
+# display spectrogram
+plt.figure(figsize=FIG_SIZE)
+librosa.display.specshow(spectrogram, sr=sample_rate, hop_length=hop_length)
+plt.xlabel("Time")
+plt.ylabel("Frequency")
+plt.colorbar()
+plt.title("Spectrogram")
+
+# apply logarithm to cast amplitude to Decibels
+log_spectrogram = librosa.amplitude_to_db(spectrogram)
+
+plt.figure(figsize=FIG_SIZE)
+librosa.display.specshow(log_spectrogram, sr=sample_rate, hop_length=hop_length)
+plt.xlabel("Time")
+plt.ylabel("Frequency")
+plt.colorbar(format="%+2.0f dB")
+plt.title("Spectrogram (dB)")
+
+
+# MFCCs
+# extract 13 MFCCs
+# Parameters for MFCC calculation
+n_fft = 2048
+hop_length = 512
+n_mfcc = 13
+
+# Calculate MFCCs
+MFCCs = librosa.feature.mfcc(y=signal, sr=sample_rate, n_fft=n_fft, hop_length=hop_length, n_mfcc=n_mfcc)
+
+# display MFCCs
+plt.figure(figsize=FIG_SIZE)
+librosa.display.specshow(MFCCs, sr=sample_rate, hop_length=hop_length)
+plt.xlabel("Time")
+plt.ylabel("MFCC coefficients")
+plt.colorbar()
+plt.title("MFCCs")
+
+
+# show plots
+# Decrease the size of the plot by changing the figure size
+smaller_fig_size = (8, 6)  # Adjust to your desired size
+plt.gcf().set_size_inches(smaller_fig_size)
+plt.show()
+
+
+
+
+# In[3]:
+
+
+#do not run after the first time!
+import json
+import os
+import math
+import librosa
+
+DATASET_PATH = "C:\\Users\\er\\AITut\\genres_original"
+JSON_PATH = "data_10.json"
+SAMPLE_RATE = 22050
+TRACK_DURATION = 30 # measured in seconds
+SAMPLES_PER_TRACK = SAMPLE_RATE * TRACK_DURATION
+
+def save_mfcc(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_length=512, num_segments=5):
+    """Extracts MFCCs from the music dataset and saves them into a JSON file along with genre labels.
+
+    :param dataset_path (str): Path to the dataset
+    :param json_path (str): Path to the JSON file used to save MFCCs
+    :param num_mfcc (int): Number of coefficients to extract
+    :param n_fft (int): Interval we consider to apply FFT. Measured in # of samples
+    :param hop_length (int): Sliding window for FFT. Measured in # of samples
+    :param num_segments (int): Number of segments we want to divide sample tracks into
+    :return:
+    """
+
+    # dictionary to store mapping, labels, and MFCCs
+    data = {
+        "mapping": [],
+        "labels": [],
+        "mfcc": []
+    }
+
+    samples_per_segment = int(SAMPLES_PER_TRACK / num_segments)
+    num_mfcc_vectors_per_segment = math.ceil(samples_per_segment / hop_length)
+
+    # loop through all genre sub-folders
+    for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
+
+        # ensure we're processing a genre sub-folder level
+        if dirpath is not dataset_path:
+
+            # save genre label (i.e., sub-folder name) in the mapping
+            semantic_label = dirpath.split("\\")[-1]
+            data["mapping"].append(semantic_label)
+            print("\nProcessing: {}".format(semantic_label))
+
+            # process all audio files in genre sub-dir
+            for f in filenames:
+                file_path = os.path.join(dirpath, f)
+
+                try:
+                    signal, sample_rate = librosa.load(file_path, sr=SAMPLE_RATE)
+                    # load audio file
+
+                    # process all segments of audio file
+                    for d in range(num_segments):
+
+                        # calculate start and finish sample for the current segment
+                        start = samples_per_segment * d
+                        finish = start + samples_per_segment
+
+                        # extract mfcc
+                        mfcc = librosa.feature.mfcc(y=signal[start:finish], sr=sample_rate, n_mfcc=num_mfcc, n_fft=n_fft, hop_length=hop_length)
+                        mfcc = mfcc.T
+
+                        # store only mfcc feature with the expected number of vectors
+                        if len(mfcc) == num_mfcc_vectors_per_segment:
+                            data["mfcc"].append(mfcc.tolist())
+                            data["labels"].append(i-1)
+                            print("{}, segment:{}".format(file_path, d+1))
+                            
+                except Exception as e:
+                    print(f"Error processing {file_path}: {str(e)}")
+
+    # save MFCCs to a JSON file
+    with open(json_path, "w") as fp:
+        json.dump(data, fp, indent=4)
+
+if __name__ == "__main__":
+    save_mfcc(DATASET_PATH, JSON_PATH, num_segments=10)
+
+
+# In[ ]:
+
+
+get_ipython().system('pip install tensorflow==1.14.0')
+
+
+# In[4]:
+
+
+import tensorflow
+
+
+# In[6]:
+
+
+import tensorflow.keras
+
+
+# In[7]:
+
+
+import json
+import numpy as np
+from sklearn.model_selection import train_test_split
+import tensorflow.keras as keras
+
+# path to json file that stores MFCCs and genre labels for each processed segment
+DATA_PATH = "C:\\Users\\er\\AITut\\data_10.json"
+
+
+def load_data(data_path):
+    """Loads training dataset from json file.
+
+        :param data_path (str): Path to json file containing data
+        :return X (ndarray): Inputs
+        :return y (ndarray): Targets
+    """
+
+    with open(data_path, "r") as fp:
+        data = json.load(fp)
+
+    # convert lists to numpy arrays
+    X = np.array(data["mfcc"])
+    y = np.array(data["labels"])
+
+    print("Data succesfully loaded!")
+
+    return  X, y
+
+
+if __name__ == "__main__":
+
+    # load data
+    X, y = load_data(DATA_PATH)
+
+    # create train/test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+    # build network topology
+    model = keras.Sequential([
+
+        # input layer
+        keras.layers.Flatten(input_shape=(X.shape[1], X.shape[2])),
+
+        # 1st dense layer
+        keras.layers.Dense(512, activation='relu'),
+
+        # 2nd dense layer
+        keras.layers.Dense(256, activation='relu'),
+
+        # 3rd dense layer
+        keras.layers.Dense(64, activation='relu'),
+
+        # output layer
+        keras.layers.Dense(10, activation='softmax')
+    ])
+
+    # compile model
+    optimiser = keras.optimizers.Adam(learning_rate=0.0001)
+    model.compile(optimizer=optimiser,
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    model.summary()
+
+    # train model
+    history = model.fit(X_train, y_train, validation_data=(X_test, y_test), batch_size=32, epochs=50)
+
+
+
+
+
+
+
+# In[ ]:
+
+
+import json
+import numpy as np
+from sklearn.model_selection import train_test_split
+import tensorflow.keras as keras
+import matplotlib.pyplot as plt
+
+# path to json file that stores MFCCs and genre labels for each processed segment
+DATA_PATH = "C:\\Users\\er\\AITut\\data_10.json"
+
+def load_data(data_path):
+    """Loads training dataset from json file.
+
+        :param data_path (str): Path to json file containing data
+        :return X (ndarray): Inputs
+        :return y (ndarray): Targets
+    """
+
+    with open(data_path, "r") as fp:
+        data = json.load(fp)
+
+    # convert lists to numpy arrays
+    X = np.array(data["mfcc"])
+    y = np.array(data["labels"])
+
+    print("Data succesfully loaded!")
+
+    return X, y
+
+
+def plot_history(history):
+    """Plots accuracy/loss for training/validation set as a function of the epochs
+
+        :param history: Training history of model
+        :return:
+    """
+
+    fig, axs = plt.subplots(2)
+
+    # create accuracy sublpot
+    axs[0].plot(history.history["accuracy"], label="train accuracy")
+    axs[0].plot(history.history["val_accuracy"], label="test accuracy")
+    axs[0].set_ylabel("Accuracy")
+    axs[0].legend(loc="lower right")
+    axs[0].set_title("Accuracy eval")
+
+    # create error sublpot
+    axs[1].plot(history.history["loss"], label="train error")
+    axs[1].plot(history.history["val_loss"], label="test error")
+    axs[1].set_ylabel("Error")
+    axs[1].set_xlabel("Epoch")
+    axs[1].legend(loc="upper right")
+    axs[1].set_title("Error eval")
+
+    plt.show()
+
+
+if __name__ == "__main__":
+
+    # load data
+    X, y = load_data(DATA_PATH)
+
+    # create train/test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+    # build network topology
+    model = keras.Sequential([
+
+        # input layer
+        keras.layers.Flatten(input_shape=(X.shape[1], X.shape[2])),
+
+        # 1st dense layer
+        keras.layers.Dense(512, activation='relu', kernel_regularizer=keras.regularizers.l2(0.001)),
+        keras.layers.Dropout(0.3),
+
+        # 2nd dense layer
+        keras.layers.Dense(256, activation='relu', kernel_regularizer=keras.regularizers.l2(0.001)),
+        keras.layers.Dropout(0.3),
+
+        # 3rd dense layer
+        keras.layers.Dense(64, activation='relu', kernel_regularizer=keras.regularizers.l2(0.001)),
+        keras.layers.Dropout(0.3),
+
+        # output layer
+        keras.layers.Dense(10, activation='softmax')
+    ])
+
+    # compile model
+    optimiser = keras.optimizers.Adam(learning_rate=0.0001)
+    model.compile(optimizer=optimiser,
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    model.summary()
+
+    # train model
+    history = model.fit(X_train, y_train, validation_data=(X_test, y_test), batch_size=32, epochs=100)
+
+    # plot accuracy and error as a function of the epochs
+    plot_history(history)
+
+
+
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
